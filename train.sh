ede13f915a3a2f10cf208c2c56f99ac79db09965
mljack <mljack@gmail.com>
2022-05-28 20:03:08 +0800

    Update dockfile for mmdet3d==1.0.0rc2 and my TransFusion clone.

---
+++ train.sh
@@ -0,0 +1,2 @@
+python -u tools/train.py configs/transfusion_nusc_voxel_L.py --auto-resume | tee train.log
+python -u tools/train.py configs/transfusion_nusc_voxel_LC.py --auto-resume | tee train.log
